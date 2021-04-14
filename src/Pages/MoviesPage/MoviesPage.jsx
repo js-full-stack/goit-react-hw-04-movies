4689a2d8d005f803e6291f30c613b1f7701bf7fe
Alex <js.fullstack.development@gmail.com>
2021-04-15 02:27:34 +0300

    upd

--- src/Pages/MoviesPage/MoviesPage.jsx
+++ src/Pages/MoviesPage/MoviesPage.jsx
@@ -7,12 +7,7 @@ import moviesApi from '../../utils/moviesApi';
 import SearchForm from '../../Components/SearchForm';
 
 const MoviesPage = () => {
-  // const [query, setQuery] = useState('');
   const [filmsList, setFilmsList] = useState([]);
-
-  // const handleInputChange = e => setQuery(e.target.value);
-
-  // useEffect(() => {}, []);
 
   const handleSubmit = searchQuery => {
     moviesApi
@@ -20,48 +15,15 @@ const MoviesPage = () => {
       .then(results => setFilmsList(results));
   };
 
-  // const
-
-  // useEffect(() => {
-  //   moviesApi.handleMovieSearch(query).then(results => setFilmsList(results));
-  // }, [query]);
-
-  // const data = moviesApi
-  //   .handleMovieSearch()
-  //   .then(results => console.log(results));
-  // console.log(data);
-
-  // useEffect(() => {
-  //   moviesApi.handleMovieSearch().then(results => setFilmsList(results));
-  // }, [query]);
-  // useEffect(() => {})
   return (
     <>
       <h2>Movies Page</h2>
       <SearchForm onSubmit={handleSubmit} />
-      {/* <form onSubmit={handleSubmit} className="SearchForm">
-        <label className="SearchLabel">
-          <input
-            type="text"
-            value={query}
-            onChange={handleInputChange}
-            className="SearchInput"
-            placeholder="Enter the title of the movie"
-          />
-          <button className="SearchButton" type="submit">
-            Search
-            <svg className="SearchIcon">
-              <use href={`${sprite}#icon-search`}></use>
-            </svg>
-          </button>
-        </label>
-      </form> */}
-
       {filmsList && (
         <ul>
-          {/* {filmsList.map(film => (
-            <li>{film.title}</li>
-          ))} */}
+          {filmsList.map(film => (
+            <li key={film.id}>{film.title}</li>
+          ))}
         </ul>
       )}
     </>
